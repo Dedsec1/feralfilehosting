1180dabfd699efa4a5b3f82fb0c20587da540860
Dedsec1 <sky.net@outlook.com>
2016-02-27 11:48:26 -0800

    1122

--- Feral Wiki/HTTP/Password protect your WWW folder/scripts/speed.sh
+++ Feral Wiki/HTTP/Password protect your WWW folder/scripts/speed.sh
@@ -68,11 +68,11 @@ reroute_log=/tmp/$(openssl rand -hex 10)
 #done
 #echo Route has been set.
 #}
-
-function error_exit {
-rm -f $reroute_log
-exit 1
-}
+#
+#function error_exit {
+#rm -f $reroute_log
+#exit 1
+#}
 
 #function requested_route_check {
 #curl -4 -s https://network.feral.io/reroute | grep checked | grep -o -P 'value=".{0,15}' | awk '{print $1}' | sed 's/value="//g' | sed 's/"//g' | sed 's/>//g'
