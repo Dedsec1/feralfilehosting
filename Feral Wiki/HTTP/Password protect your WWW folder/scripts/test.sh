c08f830f736d0cf8eb665a87e9c1f07cb2d0db2a
Dedsec1 <sky.net@outlook.com>
2016-02-27 14:05:40 -0800

    Update test.sh

--- Feral Wiki/HTTP/Password protect your WWW folder/scripts/test.sh
+++ Feral Wiki/HTTP/Password protect your WWW folder/scripts/test.sh
@@ -127,7 +127,7 @@ gitissue="https://github.com/feralhosting/feralfilehosting/issues/new"
 ############################
 #
 # Disables the built in script updater permanently by setting this variable to 0.
-updaterenabled="0"
+updaterenabled="1"
 #
 ############################
 ####### Variable End #######
@@ -394,39 +394,26 @@ then
         case "$CHOICE" in
     ##########
             "1") # Create a new ~/private/.htpasswd and user only
-               main_menu () {
-    options=(
-        "Check Your Servers's Download Speed"
-        "Check Disk IO,Disk Usage,and Current Process's Running"
-        "Restart Deluge,ruTorrent,Transmission,MySQL"
-        "Quit"
-    )
-    select option in "${options[@]}"; do
-        case $option in
-            ${options[0]})
-                Check Your Servers's Download Speed
-                break
-            ;;
-            ${options[1]})
-                Check Disk IO
-                break
-            ;;
-            ${options[2]})
-                 wget -qO ~/restart.sh http://git.io/5Uw8Gw && bash ~/restart.sh
-                    sleep 2
-                fi
-             ;;
-            ${options[3]})
-                exit
-             ;;
-            *) 
-                echo invalid option
-            ;;
-        esac
-    done
-}
-main_menu
-                fi
+                if [[ ! -f $HOME/private/.htpasswd ]]
+                then
+                        echo -e "\033[1;32m""Note: Use a good password manager like keepass so you can easily manage secure passwords." "\e[0m"
+                        read -ep "What is the username you wish to create?: " username
+                        htpasswd -cm $HOME/private/.htpasswd $username
+                        chmod 600 $HOME/private/.htpasswd
+                        echo "The .htpasswd file was created and the user: $username added"
+                        sleep 2
+                else
+                    echo -e "\033[31m""The ~/private/.htpasswd exists.""\e[0m"
+                    read -ep "Do you wish overwrite it? [y] yes or [n] no: " confirm
+                    if [[ $confirm =~ ^[Yy]$ ]]
+                    then
+                        echo -e "\033[1;32m""Note: Use a good password manager like keepass so you can easily manage secure passwords." "\e[0m"
+                        read -ep "What is the username you wish to create?: " username
+                        htpasswd -cm $HOME/private/.htpasswd $username
+                        chmod 600 $HOME/private/.htpasswd
+                        echo "The .htpasswd file was created and the user: $username added"
+                        sleep 2
+                    fi
                 fi
                 ;;
     ##########
@@ -1015,11 +1002,8 @@ main_menu
                 fi
                 ;;
     ##########
-            
-            "22") # change the rpc password for the user rutorrent-suffix of choice
-              wget -qO ~/restart.sh http://git.io/5Uw8Gw && bash ~/restart.sh
-                    sleep 2
-                fi
+            "22") # Quit
+                exit
                 ;;
     ##########
         esac
