4b249b11daf52588632c5978e31ae7dc29906c29
Dedsec1 <sky.net@outlook.com>
2016-02-27 17:53:45 -0800

    Update htpasswdtk.sh

--- Feral Wiki/HTTP/Password protect your WWW folder/scripts/htpasswdtk.sh
+++ Feral Wiki/HTTP/Password protect your WWW folder/scripts/htpasswdtk.sh
@@ -394,7 +394,7 @@ then
         case "$CHOICE" in
     ##########
             "1") # Create a new ~/private/.htpasswd and user only
-               function subopt1
+              function subopt1
 {
    subopt1=""
    while [ "$subopt1" != "x" ]
@@ -407,6 +407,39 @@ then
    done
  }
 
+ function subopt2
+ {
+     subopt2=""
+     while [ "$subopt2" != "x" ]
+     do
+         echo Sub Menu 2 Heading
+         echo Option A
+         echo Option B
+         echo x Back to Main Menu
+         read -p "Select sub-option2" subopt2    
+     done
+ }
+
+ function mainopt
+ {
+    opt=""
+    while [ "$opt" != "x" ]
+    do
+        echo Menu Heading
+        echo Sub Menu 1
+        echo Sub Menu 2
+        read -p "Select Otion: " opt
+        if [ "$opt" = "1" ]; then
+             subopt1
+        elif [ "$opt" = "2" ]; then
+             subopt2
+        elif [ "$opt" = "x" ];then
+             break
+        fi
+   done
+}
+
+mainopt
                 ;;
     ##########
             "2") # Create a new ~/private/.htpasswd,user and .htaccess.
