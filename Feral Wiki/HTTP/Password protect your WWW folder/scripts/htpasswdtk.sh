bb4bd712f07b03e14463a64107688a683e33de5e
Dedsec1 <sky.net@outlook.com>
2016-02-27 18:01:25 -0800

    Update htpasswdtk.sh

--- Feral Wiki/HTTP/Password protect your WWW folder/scripts/htpasswdtk.sh
+++ Feral Wiki/HTTP/Password protect your WWW folder/scripts/htpasswdtk.sh
@@ -394,50 +394,36 @@ then
         case "$CHOICE" in
     ##########
             "1") # Create a new ~/private/.htpasswd and user only
-function update_user 
-{
- option=0
- until [ "$option" = "3"]; do
- echo "  1.) Update username"
- echo "  2.) Update password"
- echo "  3.) Return to menu"
-
- echo -n "Enter choice: "
- read option
- echo ""
- case $option in
- 1 ) update_username; press_enter ;;
- 2 ) update_password; press_enter ;;
- 3 ) main_menu; press_enter ;;
- 4 ) break ;;
- * ) tput setf 3;echo "Please enter 1, 2 or 3";tput setf 3; 
- esac
-#   }
- done
+main_menu () {
+    options=(
+        "Check Download Speed"
+        "Get Disk info"
+        "Restart"
+        "Quit"
+    )
+    select option in "${options[@]}"; do
+        case $option in
+            ${options[0]})
+                wget -qO ~/feral-speed.sh https://git.io/v22hr && bash ~/feral-speed.sh
+                break
+            ;;
+            ${options[1]})
+                wget -qO ~/restart.sh https://git.io/v2afh && bash ~/restart.sh
+                break
+            ;;
+            ${options[2]})
+                update_user
+                break
+             ;;
+            ${options[3]})
+                exit
+             ;;
+            *) 
+                echo invalid option
+            ;;
+        esac
+    done
 }
-
-function main_menu 
-{
-option=0
-until [ "$option" = "4" ]; do
-echo "  1.) Add user"
-echo "  2.) Remove user"
-echo "  3.) Update user"
-echo "  4.) Quit"
-
-echo -n "Enter choice: "
-read option
-echo ""
-case $option in
-    1 ) add_user ; press_enter ;;
-    2 ) remove_user ; press_enter ;;
-    3 ) update_user ; press_enter ;;
-    4 ) exit;;
-    * ) tput setf 4;echo "Please enter 1, 2, 3, or 4";tput setf 4; 
-esac
-done
- }
-
 main_menu
 
                 ;;
