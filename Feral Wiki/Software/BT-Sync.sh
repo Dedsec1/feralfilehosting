9908d5b9363fe93dc3db8dd0459f25f6bd50d184
Dedsec1 <sky.net@outlook.com>
2016-02-29 10:38:02 -0800

    Create BT-Sync.sh

---
+++ Feral Wiki/Software/BT-Sync.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+########################
+####Define Variables####
+########################
+echo "Setting up Bittorrent Sync."
+wget -qO ~/install.btsync http://git.io/zHDQGA && bash ~/install.btsync
+##
