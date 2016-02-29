62401563a8a3703543ccd11458a48a2aa8b8913c
Dedsec1 <sky.net@outlook.com>
2016-02-29 10:48:51 -0800

    Update CMAKE.sh

--- Feral Wiki/Software/CMAKE - Basic Setup/CMAKE.sh
+++ Feral Wiki/Software/CMAKE - Basic Setup/CMAKE.sh
@@ -2,7 +2,6 @@
 ########################
 ####Define Variables####
 ########################
-echo "Setting up CMAKE."
 mkdir -p ~/bin && bash
 wget -qO ~/cmake.tar.gz http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
 tar xf ~/cmake.tar.gz --strip-components=1 -C ~/
