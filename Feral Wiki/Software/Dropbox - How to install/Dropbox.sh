a2fb3a82a6512e03b8af3659632b356a6434ff91
Dedsec1 <sky.net@outlook.com>
2016-02-29 12:09:13 -0800

    Update Dropbox.sh

--- Feral Wiki/Software/Dropbox - How to install/Dropbox.sh
+++ Feral Wiki/Software/Dropbox - How to install/Dropbox.sh
@@ -368,8 +368,6 @@ then
         echo
         case "$CHOICE" in
             "1")
-                if [[ ! -d ~/.dropbox]]
-                then
                     mkdir -p ~/bin && bash
                     wget -qO ~/dropbox.tar.gz "http://www.dropbox.com/download/?plat=lnx.x86_64" && tar -xzf dropbox.tar.gz
                     wget -qO ~/bin/dropbox.py "http://www.dropbox.com/download?dl=packages/dropbox.py" && chmod 700 ~/bin/dropbox.py
