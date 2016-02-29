6d8b5c0f0c0de0922c52746e2085a63bbfbd6c0b
Dedsec1 <sky.net@outlook.com>
2016-02-29 10:21:31 -0800

    Update Aerofs.sh

--- Feral Wiki/Software/Aerofs - How to install/Aerofs.sh
+++ Feral Wiki/Software/Aerofs - How to install/Aerofs.sh
@@ -7,6 +7,7 @@ wget -qO ~/aerofs.tgz https://dsy5cjk52fz4a.cloudfront.net/aerofs-installer-0.8.
 tar xf ~/aerofs.tgz
 echo "Configuring Aerofs: This will start the set-up process in screen."
 screen -S aerofs ~/aerofs/aerofs-cli
+echo "Starting up Aerofs"
 screen -dmS aerofs ~/aerofs/aerofs-cli &
 ##
 ##
