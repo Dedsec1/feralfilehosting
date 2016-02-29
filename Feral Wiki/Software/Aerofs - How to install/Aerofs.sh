abf0edbb559fccb8706a05319542bc14f20347a8
Dedsec1 <sky.net@outlook.com>
2016-02-29 09:50:04 -0800

    Update Aerofs.sh

--- Feral Wiki/Software/Aerofs - How to install/Aerofs.sh
+++ Feral Wiki/Software/Aerofs - How to install/Aerofs.sh
@@ -7,6 +7,7 @@ wget -qO ~/aerofs.tgz https://dsy5cjk52fz4a.cloudfront.net/aerofs-installer-0.8.
 tar xf ~/aerofs.tgz
 echo "Configuring Aerofs: This will start the set-up process.
 
+
 Enter the email you registered with when prompted.
 
 Enter you password for this account when prompted.
@@ -15,6 +16,7 @@ Enter the path you want for you Aerofs folder when prompted. This path will ~/Ae
 
 When the set-up is complete press this keyboard sequence to detach from the screen:
 
-Press and hold CTRL and A then press D
-Once you have setup up Aerofs you can start it easily using this command: screen -dmS aerofs ~/aerofs/aerofs-cli &"
+Press and hold CTRL and A then press D"
+screen -S aerofs ~/aerofs/aerofs-cli
+echo Once you have setup up Aerofs you can start it easily using this command: screen -dmS aerofs ~/aerofs/aerofs-cli &"
 ##
